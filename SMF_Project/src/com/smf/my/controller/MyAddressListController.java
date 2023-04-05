915cecaee38aeebfb30cc0e91130067986e0f59c
SSHwan22 <113297372+SSHwan22@users.noreply.github.com>
2023-04-05 14:17:09 +0900

    Update MyAddressListController.java

--- SMF_Project/src/com/smf/my/controller/MyAddressListController.java
+++ SMF_Project/src/com/smf/my/controller/MyAddressListController.java
@@ -38,7 +38,7 @@ public class MyAddressListController extends HttpServlet {
 		ArrayList<Address> addrList = new AddressService().addressList();
 		
 		request.setAttribute("addrDefault", addrDefault);
-		request.setAttribute("addrList", addrList);
+		request.setAttribute("addrList", addrList);dsfadf
 		
 		request.getRequestDispatcher("/views/my/mypageAddress.jsp").forward(request, response);
 	}
