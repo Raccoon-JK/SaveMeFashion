ffc917a8a4e401a8dc70017a03f3807fe49989fb
SSU <113297372+SSHwan22@users.noreply.github.com>
2023-04-12 02:38:34 +0900

    Revert "장바구니 DB연결"
    This reverts commit 5b8f556eee428ab675eda6b4c19d5effbd0bf7ce.

--- SMF_Project/src/com/smf/my/controller/MyWishListDeleteController.java
+++ SMF_Project/src/com/smf/my/controller/MyWishListDeleteController.java
@@ -7,9 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.smf.member.model.vo.Member;
-import com.smf.my.model.service.MyPageService;
-
 /**
  * Servlet implementation class MyWishListDeleteController
  */
@@ -29,21 +26,9 @@ public class MyWishListDeleteController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
 		
-		String userId = ((Member)request.getSession().getAttribute("loginUser")).getUserId();
-		String pName = request.getParameter("pName");
 		
-		int result = new MyPageService().wishlistItemDelete(userId, pName);
-		
-		if( result>0 ) {
-			
-			request.getSession().setAttribute("alertMsg", "삭제되었습니다.");
-			
-			response.sendRedirect(request.getContextPath()+"/mypagewishlist.me");
-		}else {
-			System.out.println("삭제 오류!");
-		}
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
